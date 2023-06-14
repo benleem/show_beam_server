55c05cc5b7a0a4ba1ca54f78eca4174e2bb1fd40
voidKandy <voidkandy@gmail.com>
2023-06-14 15:50:01 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use actix_web::{web, App, HttpServer, Result};
+use sqlx::Connection;
+use std::env;
+#[actix_web::main]
+pub async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+    let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let conn = sqlx::MySqlConnection::connect(&url).await.unwrap();
+    println!("{:?}", url);
+    Ok(())
 }
