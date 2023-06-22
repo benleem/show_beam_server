87f7bc0bf212d21d2284b1a4a3cee5bec9c00e94
Ben Marshall <benleem98@yahoo.com>
2023-06-22 17:39:07 -0400

    building some shows endpoints

--- src/handlers/shows.rs
+++ src/handlers/shows.rs
@@ -1,38 +1,78 @@
-use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
+use crate::{
+    models::shows::{
+        CreateShowSchema, DeleteShowSchema, GetUserShowsSchema, ShowModel, UpdateShowSchema,
+    },
+    AppState,
+};
+
+use actix_web::{
+    delete, get, post, put,
+    web::{self, Data, Json, Path, Query},
+    HttpResponse, Responder,
+};
 use serde_json::json;
 
-#[get("")]
-async fn get_all_shows() -> impl Responder {
-    const MESSAGE: &str = "Hello, world!";
+// endpoints
 
-    HttpResponse::Ok().json(json!({ "message": MESSAGE }))
+#[get("")]
+async fn get_all_shows(data: Data<AppState>) -> impl Responder {
+    let shows: Vec<ShowModel> = sqlx::query_as!(ShowModel, "SELECT * FROM shows")
+        .fetch_all(&data.db)
+        .await
+        .unwrap();
+    let json_response = serde_json::json!({ "shows": shows });
+    HttpResponse::Ok().json(json!(json_response))
+}
+
+#[get("/{id}")]
+async fn get_show_by_id(path: Path<(String,)>, data: Data<AppState>) -> HttpResponse {
+    let show: ShowModel = sqlx::query_as!(
+        ShowModel,
+        "SELECT * FROM shows WHERE id = ?",
+        path.into_inner().0
+    )
+    .fetch_one(&data.db)
+    .await
+    .unwrap();
+    let json_response = serde_json::json!({ "show": show });
+    HttpResponse::Ok().json(json!(json_response))
+}
+
+#[get("/users/{id}")]
+async fn get_all_user_shows(
+    path: Path<(String,)>,
+    params: Query<GetUserShowsSchema>,
+    data: Data<AppState>,
+) -> impl Responder {
+    // if params.favorites {
+    //     //do something
+    // }
+    let shows: Vec<ShowModel> = sqlx::query_as!(
+        ShowModel,
+        "SELECT * FROM shows WHERE owner = ?",
+        path.into_inner().0
+    )
+    .fetch_all(&data.db)
+    .await
+    .unwrap();
+    let json_response = serde_json::json!({ "shows": shows });
+    HttpResponse::Ok().json(json!(json_response))
 }
 
 #[post("")]
-async fn new_show() -> impl Responder {
-    const MESSAGE: &str = "Hello, world!";
-
-    HttpResponse::Ok().json(json!({ "message": MESSAGE }))
+async fn new_show(body: Json<CreateShowSchema>, data: Data<AppState>) -> impl Responder {
+    HttpResponse::Ok().json(json!(body))
 }
 
 #[put("")]
-async fn edit_show() -> impl Responder {
+async fn edit_show(body: Json<UpdateShowSchema>, data: Data<AppState>) -> impl Responder {
     const MESSAGE: &str = "Hello, world!";
 
     HttpResponse::Ok().json(json!({ "message": MESSAGE }))
 }
 
 #[delete("")]
-async fn delete_show() -> impl Responder {
-    const MESSAGE: &str = "Hello, world!";
-
-    HttpResponse::Ok().json(json!({ "message": MESSAGE }))
-}
-
-// /shows/users
-
-#[get("/users")]
-async fn get_all_user_shows() -> impl Responder {
+async fn delete_show(params: Query<DeleteShowSchema>, data: Data<AppState>) -> impl Responder {
     const MESSAGE: &str = "Hello, world!";
 
     HttpResponse::Ok().json(json!({ "message": MESSAGE }))
@@ -43,10 +83,10 @@ async fn get_all_user_shows() -> impl Responder {
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/shows")
         .service(get_all_shows)
+        .service(get_show_by_id)
         .service(new_show)
         .service(edit_show)
         .service(delete_show)
         .service(get_all_user_shows);
-
     conf.service(scope);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod handlers;
 use handlers::shows;
+mod models;
 
 use actix_cors::Cors;
 use actix_web::middleware::Logger;
@@ -18,14 +19,19 @@ async fn main() -> std::io::Result<()> {
     env_logger::init(); // logging api activity, good for
 
     // let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let database_url = std::env::var("DEV_DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = std::env::var("DEV_DATABASE_URL").expect("DEV_DATABASE_URL must be set");
+    // let base_url = std::env::var("BASE_URL").expect("BASE_URL must be set");
+    let base_url = std::env::var("DEV_BASE_URL").expect("DEV_BASE_URL must be set");
+    let port = std::env::var("PORT").expect("PORT must be set");
+    let port = port.parse::<u16>().unwrap();
+
     let pool = match MySqlPoolOptions::new()
-        .max_connections(10)
+        .max_connections(5)
         .connect(&database_url)
         .await
     {
         Ok(pool) => {
-            println!("✅Connection to the database is successful!");
+            println!("✅ Connection to the database is successful!");
             pool
         }
         Err(err) => {
@@ -52,7 +58,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors)
             .wrap(Logger::default())
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((base_url, port))?
     .run()
     .await
 }
