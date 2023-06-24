caa0dabf989590303373e092d504e110df9f4815
voidKandy <voidkandy@gmail.com>
2023-06-24 14:47:43 -0700

    Updated User models

--- src/models/users.rs
+++ src/models/users.rs
@@ -1,25 +1,62 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, sqlx::FromRow)]
-pub struct User {
-    id: String,
-    username: String,
-    email: String,
+pub struct UserModel {
+    pub id: String,
+    pub name: String,
+    pub email: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct CreateUser {
-    id: String,
-    username: String,
-    email: String,
+    pub id: String,
+    pub username: String,
+    pub email: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DeleteUser {
-    id: String,
+    pub id: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize)]
+pub struct FilteredUser {
+    pub id: String,
+    pub name: String,
+    pub email: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct UserData {
+    pub user: FilteredUser,
+}
+
+#[derive(Serialize, Debug)]
 pub struct UserResponse {
-    id: String,
+    pub status: String,
+    pub data: UserData,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RegisterUserSchema {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LoginUserSchema {
+    pub email: String,
+    pub password: String,
+}
+
+impl UserModel {
+    pub fn user_to_response(user: &UserModel) -> FilteredUser {
+        FilteredUser {
+            id: user.id.to_owned(),
+            name: user.name.to_owned(),
+            email: user.email.to_owned(),
+        }
+    }
 }
