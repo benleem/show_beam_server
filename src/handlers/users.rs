c611e99d75098f5e6e0c1e745820a8ea28cb5f03
voidKandy <voidkandy@gmail.com>
2023-06-24 14:48:38 -0700

    Updated handlers for auth and users, shows completely commented out. Expecting complete overrite when merged with main

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -1,14 +1,35 @@
-use crate::AppState;
+use crate::models::auth::AppState;
+use crate::models::users::UserModel;
+use actix_web::dev::Path;
+use actix_web::web::Data;
 use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse, Responder};
 use serde_json::json;
-use sqlx::mysql;
+use sqlx;
 
 #[get("")]
-async fn get_all_users() -> impl Responder {
+async fn get_all_users(_data: Data<AppState>) -> impl Responder {
     const MESSAGE: &str = "Hello from get all users";
-    const data: mysql::MySqlPool = HttpRequest::app_data::<mysql::MySqlPool>;
 
     HttpResponse::Ok().json(json!({ "message": MESSAGE }))
+}
+
+#[get("/{id}")]
+async fn get_user_by_id(path: web::Path<(String,)>, data: web::Data<AppState>) -> impl Responder {
+    match sqlx::query_as::<_, UserModel>("SELECT * FROM users WHERE id = ?")
+        .bind(&path.0)
+        .fetch_optional(&data.db)
+        .await
+    {
+        Ok(Some(user)) => {
+            let json_response = json!({ "user": user });
+            HttpResponse::Ok().json(json_response)
+        }
+        Ok(None) => HttpResponse::NotFound().body("User not found"),
+        Err(error) => {
+            eprintln!("Failed to retrieve user: {}", error);
+            HttpResponse::InternalServerError().body("Failed to retrieve user")
+        }
+    }
 }
 
 #[post("")]
