37a90ddb911d5eb53546eedf24d63866a41a3345
voidKandy <voidkandy@gmail.com>
2023-11-21 09:56:01 -0800

    updated migrations for new DB

--- migrations/20230629221152_users.up.sql
+++
@@ -1,7 +0,0 @@
--- Add up migration script here
-CREATE TABLE
-    IF NOT EXISTS users (
-        id CHAR(36) PRIMARY KEY NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        name VARCHAR(255) NOT NULL
-    );
